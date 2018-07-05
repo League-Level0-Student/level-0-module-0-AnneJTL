fb4125d5791c62faeb3b1c01f4efd182b8a523dc
AnneJTL <anne.chatin@jointheleague.org>
2018-07-05 11:41:11 -0700

    pourquoi les 2 premiers sont la ?

---
+++ src/MorningZombie.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MorningZombie {
+
+    public static void main(String[] args) {
+
+        String myRoutine = "1 - caresser le chat\n2 - petit dej\n" +
+                "3 - douche";
+        JOptionPane.showMessageDialog(null, myRoutine, "\tMy Routine", JOptionPane.PLAIN_MESSAGE);
+    }
+}
